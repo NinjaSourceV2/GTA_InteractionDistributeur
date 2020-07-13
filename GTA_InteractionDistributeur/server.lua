3e733a1a6440f1d71764a7b7d5f5502f08e32e7d
Ninja <samir932205@gmail.com>
2020-07-13 21:52:55 +0200

    1.1 master

--- GTA_InteractionDistributeur/server.lua
+++ GTA_InteractionDistributeur/server.lua
@@ -1,6 +1,6 @@
 --> Version de la Resource : 
 local LatestVersion = ''; CurrentVersion = '1.1'
-PerformHttpRequest('https://raw.githubusercontent.com/NinjaSourceV2/GTA_InteractionDistributeur/master/VERSION', function(Error, NewestVersion, Header)
+PerformHttpRequest('https://raw.githubusercontent.com/NinjaSourceV2/GTA_InteractionDistributeur/master/GTA_InteractionDistributeur/VERSION', function(Error, NewestVersion, Header)
     LatestVersion = NewestVersion
     if CurrentVersion ~= NewestVersion then
         print("\n\r ^2[GTA_InteractionDistributeur]^1 La version que vous utilisé n'est plus a jours, veuillez télécharger la dernière version. ^3\n\r")
